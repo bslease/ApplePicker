0b44719aa099abf14505fede514fa911f09cd7cf
bslease <bslease@gmail.com>
2021-04-16 15:00:48 -0700

    add score counting

--- Apple Picker Prototype/Assets/Basket.cs
+++ Apple Picker Prototype/Assets/Basket.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
+    private void Start()
+    {
+        // Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        // Get the Text Component of that GameObject
+        scoreGT = scoreGO.GetComponent<Text>();
+        // Set the starting number of points to 0
+        scoreGT.text = "0";
+    }
+
     void Update()
     {
         // Get the current screen position of the mouse from Input
@@ -31,6 +45,13 @@ public class Basket : MonoBehaviour
         if (collideWith.tag == "Apple")
         {
             Destroy(collideWith);
+
+            // Parse the text of the scoreGT into an int
+            int score = int.Parse(scoreGT.text);
+            // Add points for catching the apple
+            score += 100;
+            // Convert the score back to a string and display it
+            scoreGT.text = score.ToString();
         }
     }
 }
